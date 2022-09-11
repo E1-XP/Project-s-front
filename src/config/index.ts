d0874e5cdc2630dd276cb1877c89ccdc951ec0e7
E1-XP <poczta.elektroniczna@hotmail.com>
2022-09-11 16:28:03 +0200

    use netlify proxy

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 export default {
   API_URL:
     process.env.NODE_ENV === 'production'
-      ? 'https://project-s.gtxcodeworks.site'
-      : 'http://localhost:3001',
+      ? `${window.location.origin}/api`
+      : 'https://project-s.gtxcodeworks.site',
 };
