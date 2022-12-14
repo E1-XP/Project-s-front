6ec990bb390349bf1b8610a0cf670dca2c6a69e5
E1-XP <poczta.elektroniczna@hotmail.com>
2022-12-14 16:48:08 +0100

    update api url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 export default {
   API_URL:
     process.env.NODE_ENV === 'production'
-      ? 'https://project-s.gtxcodeworks.site'
+      ? 'https://project-s.gtxcodeworks.online'
       : 'http://localhost:3001',
 };
